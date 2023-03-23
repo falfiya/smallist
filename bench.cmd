1dce7abf23ee9902232c52f85d043a4d601087e6
coalpha <22552895+coalpha@users.noreply.github.com>
2023-03-23 02:51:34 -0700

    updated benchmarking

--- bench.cmd
+++ bench.cmd
@@ -13,7 +13,7 @@ for /f %%c in ('copy /z %~f0 nul') do (
       timeit wmic process get name,processid,parentprocessid > nul 2> nul
       timeit fastlist-0.3.0-x64.exe > nul 2> nul
       timeit smallist.exe > nul 2> nul
-      @REM timeit smallist32.exe > nul 2> nul
+      timeit smallist32.exe > nul 2> nul
       timeit fastlist_fastio.exe > nul 2> nul
       timeit fastlist-rs.exe > nul 2> nul
       < nul set /p=%%c
